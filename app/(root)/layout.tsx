c6f66baf54a9765fee77d8477230f9e022e63751
Ankur1493 <ankursharma1493@gmail.com>
2024-03-15 16:01:30 +0530

    implemented left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import LeftSidebar from '@/components/shared/LeftSidebar'
 import Navbar from '@/components/shared/navbar/Navbar'
 import React from 'react'
 
@@ -6,8 +7,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className='background-light850_dark100 relative'>
       <Navbar />
       <div className='flex'>
-        Left Sidebar
-
+        <LeftSidebar />
         <section className='flex min-h-screen flex-1 flex-col'>
           <div>{children}</div>
         </section>
